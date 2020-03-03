b0a2922c755a6d0431b02f2a36d9eea0adbde434
David Barrow <dave@fusion-arc.uk>
2020-03-03 02:24:15 +0000

    logins build

--- projects/views.py
+++ projects/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from projects.models import Project
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, View
+from django.shortcuts import redirect
+from django.utils import timezone
+
+
+def index(request):
+    return render(request, 'index')
+
 
 def project_index(request):
     projects = Project.objects.all()
     context = {
-        'projects' : projects
+        'projects': projects
     }
-    
+
     return render(request, 'project_index.html', context)
 
 
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     context = {
-        'project' : project
+        'project': project
     }
-    
+
     return render(request, 'project_detail.html', context)
